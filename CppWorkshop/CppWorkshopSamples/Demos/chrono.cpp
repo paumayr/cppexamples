559b98f0c731d8d0bfb9efbac1d7944d10c8f845
Philipp Aumayr <philipp@software-architects.at>
2013-11-04 14:17:58 +0100

    chrono sample

---
+++ CppWorkshop/CppWorkshopSamples/Demos/chrono.cpp
@@ -0,0 +1,26 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include <chrono>
+#include <thread>
+#include <ctime>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+
+TEST_CLASS(TestChrono)
+{
+	TEST_METHOD(TestSystemClock)
+	{
+		using namespace std::chrono;
+
+		auto p1 = system_clock::now();
+		this_thread::sleep_for(seconds(10));
+		auto p2 = system_clock::now();
+
+		auto dif = duration_cast<milliseconds>(p2 - p1).count();
+
+		Assert::IsTrue(dif >= 10000);
+	}
+};
